dbfc20409a2ad1720e11e32e2075973733a4b4d9
Tomáš Milata <tomas.milata@gmail.com>
2019-04-09 00:07:44 +0200

    Add a plugin to setup compiler options

---
+++ src/main/scala/ai/faculty/sbt/ScalaCompilerOptionsPlugin.scala
@@ -0,0 +1,24 @@
+package ai.faculty.sbt
+
+import sbt.Keys.{console, scalacOptions}
+import sbt._
+import sbt.plugins.JvmPlugin
+
+object ScalaCompilerOptionsPlugin extends AutoPlugin {
+
+  override def trigger = allRequirements
+  override def requires = JvmPlugin
+
+  override lazy val projectSettings = Seq(
+    scalacOptions ++= Seq(
+      "-feature",
+      "-Ywarn-unused-import",
+      "-Ywarn-dead-code",
+      "-Ywarn-unused",
+      "-Ywarn-value-discard",
+      "-Yno-adapted-args",
+      "-Xlint:_"
+    ),
+    scalacOptions in (Compile, console) ~= (_ filterNot (_ == "-Ywarn-unused-import"))
+  )
+}
