ccb75af899ca28c88b86502263154db224b18dda
Tomáš Milata <tomas@faculty.ai>
2019-07-26 12:50:45 +0200

    Add scripted tests

---
+++ src/sbt-test/sbt-house-rules/compile/src/main/scala/Main.scala
@@ -0,0 +1,7 @@
+package test
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println(args)
+  }
+}
---
+++ src/sbt-test/sbt-house-rules/integration-test/src/it/scala/DummyIntegrationTest.scala
@@ -0,0 +1,8 @@
+package test
+
+class DummyIntegrationTest extends FlatSpec with Matchers {
+
+  "integration test" should "work" in {
+    println("dummy integration test succeeded")
+  }
+}
